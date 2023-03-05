9a579e4b159fb333d9b4ac584dea93a0da788615
Gabriel Davi <gabrieldavi@MacBook-Air-de-Gabriel.local>
2023-03-05 15:28:45 -0300

    base

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Testing tsup')
+import { ApolloServer } from '@apollo/server';
+import { buildSchema } from 'graphql'
+import {importSchema} from 'graphql-import'
+import { expressMiddleware } from '@apollo/server/express4';
+import { json } from 'body-parser';
+import path from 'path';
+import express from 'express';
+import { resolvers } from './resolvers';
+console.log(path.join(__dirname, './schemas/schema.gql'));
+
+const app = express();
+(async () => {
+  const server = new ApolloServer<any>({
+    typeDefs: buildSchema(importSchema(path.join(__dirname, './schemas/schema.gql'))),
+    resolvers
+  });
+  await server.start();
+
+app.use('/graphql',  json(), expressMiddleware(server));
+app.listen(3000)
+})()
+
